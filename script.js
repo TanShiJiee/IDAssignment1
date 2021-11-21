2589acd6e013c817331042efe34ed8d120965356
TanShiJiee <S10223344@connect.np.edu.sg>
2021-11-21 14:43:48 +0800

    comment out console log

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const checkCerts = ["#portfolio-cert1", "#portfolio-cert2", "#portfolio-cert3", 
 "#portfolio-cert4", "#portfolio-cert5", "#portfolio-cert6", "#portfolio-cert7"];
 
-console.log(checkCerts.length);
+//console.log(checkCerts.length);
 /* when you click the button, index=1, if index<1 make index=7 (slide 7--> LAST SLIDE ). 
 if index is more than 7 make index=1 (Go back to SLIDE 1).
 make loop and change opacity*/
@@ -20,21 +20,21 @@ function slidesTurn(n){
     if (index>6){
         index = 0;
     }
-    console.log("index: "+index);
-    console.log(checkCerts[index]);
+    //console.log("index: "+index);
+    //console.log(checkCerts[index]);
     for (i=0; i<checkCerts.length; i++){
         if (i==index){
             slidesPosition= document.querySelector(checkCerts[i])
             slidesPosition.style.opacity=1
 
-            console.log(slidesPosition)
+            //console.log(slidesPosition)
             continue
             
         }
         else{
             slidesPosition= document.querySelector(checkCerts[i])
             slidesPosition.style.opacity=0
-            console.log(i) 
+            //console.log(i) 
         }
         
 
